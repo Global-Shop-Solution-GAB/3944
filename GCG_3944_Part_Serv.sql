84ed14a45c4f16476841a7e1caa23da7c8aa36fb
Service Web <sw@gssmail.com>
2024-03-11 10:00:59 -0500

    Commit changes in C:\Websites\Git\emarlow\3944\GCG_3944_Part_Serv.sql

---
+++ GCG_3944_Part_Serv.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_3944_Part_Serv"(
+ "IT_Tag_ID" CHAR(50),
+ "Emp_Num" CHAR(5),
+ "User_ID" CHAR(3),
+ "Service_Date" DATE,
+ "Issue" CHAR(250),
+ "Warranty_Date" DATE,
+ "Rec_ID" CHAR(50));
