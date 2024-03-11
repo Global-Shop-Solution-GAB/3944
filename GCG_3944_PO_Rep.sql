ca37821a86e320176b812e7933cfd2b310ec2823
Service Web <sw@gssmail.com>
2024-03-11 10:00:59 -0500

    Commit changes in C:\Websites\Git\emarlow\3944\GCG_3944_PO_Rep.sql

---
+++ GCG_3944_PO_Rep.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3944_PO_Rep"(
+ "PO_Num" CHAR(7),
+ "Part_Num" CHAR(20),
+ "Ven_Name" CHAR(30),
+ "Ven_Num" CHAR(6),
+ "Quantity" NUMERIC(14,2),
+ "Price" NUMERIC(15,2),
+ "Date_Due" CHAR(10),
+ "BIRUNID" BIGINT);
