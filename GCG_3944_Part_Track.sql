2aadf65e89e7c7eb01acd8d67168ea5e53e54957
Service Web <sw@gssmail.com>
2024-03-11 10:00:59 -0500

    Commit changes in C:\Websites\Git\emarlow\3944\GCG_3944_Part_Track.sql

---
+++ GCG_3944_Part_Track.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3944_Part_Track"(
+ "IT_Part_Num" CHAR(20),
+ "IT_Tag_ID" CHAR(20),
+ "Employee_Num" CHAR(5),
+ "Assigned_Date" DATE,
+ "Warranty_Start" DATE,
+ "Unassign_Date" DATE,
+ "Warranty_Length" INTEGER,
+ "Relation_ID" INTEGER);
