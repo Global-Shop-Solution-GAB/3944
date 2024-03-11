249322e9a7f0360f61461d898c0f662d68ad787f
Service Web <sw@gssmail.com>
2024-03-11 10:00:58 -0500

    Commit changes in C:\Websites\Git\emarlow\3944\GCG_3944_Asset_Rep.sql

---
+++ GCG_3944_Asset_Rep.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "GCG_3944_Asset_Rep"(
+ "Part_Num" CHAR(20),
+ "Tag_ID" CHAR(20),
+ "Curr_Assign" CHAR(5),
+ "Make" CHAR(30),
+ "Model" CHAR(30),
+ "ProdKey" CHAR(50),
+ "Assign_His" CHAR(5),
+ "BIRUNID" BIGINT,
+ "Flag" CHAR(1));
